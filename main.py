aeb5b7fd09552e85c66da9d71fd95d8f1b56fb21
YogiWaghmare <waghmareyogi295@gmail.com>
2026-04-17 15:03:35 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello Data Science Project")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operator")
